17287c991941569f6d6d9c9f4345fc37be3b15e2
Ethan Hayes <freyestorres@usf.edu>
2026-02-12 13:04:23 -0500

    Update incollege.cob

--- src/incollege.cob
+++ src/incollege.cob
@@ -632,13 +632,8 @@
                    WHEN 5
                        PERFORM LEARN-NEW-SKILL
                    WHEN 9
-                       MOVE " " TO LINE-TEXT
-                       PERFORM PRINT-LINE
-                       PERFORM START-SCREEN
-
-                       CLOSE INPUT-FILE
-                       CLOSE OUTPUT-FILE
-                       STOP RUN
+                       SET NOT-LOGGED-IN TO TRUE
+                       EXIT PERFORM
 
                    WHEN OTHER
                        MOVE "Invalid choice. Try again." TO LINE-TEXT
